be2e57ac374d2e15df2b55309a708ab5607cae12
Ibrahiim10 <ustadibrahim00@gmail.com>
2026-01-24 10:16:53 +0300

    Creating PDF Chunks for Processing

--- lib/document-processor.ts
+++ lib/document-processor.ts
@@ -27,10 +27,10 @@ export async function processDocument(
         throw new Error(`Unsupported file type: ${fileType}`);
     }
 
-    // TODO: Create chunks from the content
-
-    // TODO:
-    return { content: '', chunks: [] };
+    // Create chunks from the content
+    const chunks = await createChunks(content, file.name);
+    // process the document based on the file type
+    return { content, chunks };
   } catch (error) {
     console.error('Error processing document:', error);
     throw error;
@@ -73,4 +73,38 @@ async function processPDF(file: File): Promise<string> {
     console.error('Error processing PDF:', error);
     throw error;
   }
+}
+
+async function createChunks(
+  content: string,
+  filename: string,
+): Promise<string[]> {
+  let cleanContent = content.trim().replace(/  +/g, '');
+
+  const { RecursiveCharacterTextSplitter } =
+    await import('@langchain/textsplitters');
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 2000,
+    chunkOverlap: 400,
+    separators: [
+      // order
+      '\n\n\n',
+      '\n\n',
+      '\n',
+      '. ',
+      ' ',
+      '',
+    ],
+  });
+
+  const rawChunks = await splitter.splitText(cleanContent);
+
+  return rawChunks.map((chunk) => addContext(chunk, filename));
+}
+
+function addContext(chunk: string, filename: string): string {
+  const docName = filename.replace(/\.[^/.]+$/, '');
+
+  return `Document : ${document} \n\n ${chunk}`;
 }
